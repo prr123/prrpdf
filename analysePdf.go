88fadc4831ef67db7aa103a365358024f427331d
prr123 <priemen@gmail.com>
2022-11-18 18:31:30 +0100

    some changes

--- analysePdf.go
+++ analysePdf.go
@@ -10,14 +10,14 @@ import (
 	"os"
 	"fmt"
 	"strings"
-	"prrpdf/pdflib"
+	"prrpdf/pdfLib"
+	util "prrpdf/utilLib"
 )
 
 
 func main() {
 
 	numArgs:= len(os.Args)
-	fmt.Printf("num Args: %d\n", numArgs)
 
 	if numArgs < 2 {
 		fmt.Printf("error - exit: insufficient command line arguments\n")
@@ -26,29 +26,30 @@ func main() {
 
 	parseFilnam :=os.Args[1]
 
+	flags := [] string {"out", "dbg"}
+
+	argmap, err := util.ParseFlagsStart(os.Args, flags,2)
+	if err != nil {fmt.Printf("error ParseFlags: %v\n", err); os.Exit(-1);}
+
+	outFilNam, ok := argmap["out"]
+	if !ok {
+   		pos := strings.Index(parseFilnam, ".pdf")
+    	if pos == -1 {fmt.Printf("error parseFilnam has no pdf extension!\n"); os.Exit(-1);}
+		outFilNam = parseFilnam[0:(pos+1)] + "pdfdat"
+	}
+
+	outFilNamStr := outFilNam.(string)
+fmt.Printf("out file: %s\n",outFilNamStr)
+
 	pdf := pdflib.Init()
 
-	err := pdf.ReadPdf(parseFilnam)
-	if err != nil {
-		fmt.Printf("error ReadPdf file: %s! %v\n", parseFilnam, err)
-		os.Exit(2)
-	}
+	err = pdf.ReadPdf(parseFilnam)
+	if err != nil {fmt.Printf("error ReadPdf file: %s! %v\n", parseFilnam, err); os.Exit(-1);}
 
 	pdf.PrintPdf()
 
-   	pos := strings.Index(parseFilnam, ".pdf")
-    if pos == -1 {
-        fmt.Printf("error parseFilnam has no pdf extension!\n")
-		os.Exit(2)
-    }
+	err = pdf.CheckPdf(outFilNamStr)
+	if err != nil {fmt.Printf("error CheckPdf: %v\n", err); os.Exit(-1);}
 
-	outfilnam := parseFilnam[0:pos]
-	err = pdf.AnalysePdf(outfilnam)
-	if err != nil {
-		fmt.Printf("error ParsePdf: %v\n", err)
-		os.Exit(2)
-	}
-
-
-	fmt.Println("success!")
+	fmt.Println("success checkPdf!")
 }
